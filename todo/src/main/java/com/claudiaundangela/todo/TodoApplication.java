8205a5b02c90c0415d95c5a9ae648a9ffe86a95f
claudiacolas <claudiacolastorres@gmail.com>
2023-12-17 00:41:32 +0100

    Deleting redundant

--- todo/src/main/java/com/claudiaundangela/todo/TodoApplication.java
+++ todo/src/main/java/com/claudiaundangela/todo/TodoApplication.java
@@ -1,16 +1,8 @@
 package com.claudiaundangela.todo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-
-
-import java.util.List;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.claudiaundangela.todo.config")
